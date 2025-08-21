de0d68ffe4761209fea795aafc48b2f2159f6d4a
David Castejon <ombengue@hotmail.com>
2025-08-21 12:26:53 +0200

    Remove unnecessary imports

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // IMPORTS
 const express = require("express");
 const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require("dotenv");
 const cors = require("cors");
 
